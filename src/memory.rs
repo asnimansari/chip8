9bbebd262896475d65eeecc854eef02db2acd7c7
asnimansari <asnim96@gmail.com>
2020-02-14 00:31:17 +0530

    Stored Sprites to memory

--- src/memory.rs
+++ src/memory.rs
@@ -10,8 +10,132 @@ impl Memory {
     pub fn read_byte(&mut self, address: u16) {}
 
     pub fn new() -> Memory {
-        Memory {
+        let mut memory = Memory {
             mem: [0; 4096]
+        };
+        let sprites = [
+            [
+                0xF0,
+                0x90,
+                0x90,
+                0x90,
+                0xF0
+            ],
+            [
+                0x20,
+                0x60,
+                0x20,
+                0x20,
+                0x70
+            ],
+            [
+                0xF0,
+                0x10,
+                0xF0,
+                0x80,
+                0xF0
+            ],
+            [
+                0xF0,
+                0x10,
+                0xF0,
+                0x10,
+                0xF0
+            ],
+            [
+                0x90,
+                0x90,
+                0xF0,
+                0x10,
+                0x10,
+            ],
+            [
+                0xF0,
+                0x80,
+                0xF0,
+                0x10,
+                0xF0
+            ],
+            [
+                0xF0,
+                0x80,
+                0xF0,
+                0x90,
+                0xF0,
+            ],
+            [
+                0xF0,
+                0x10,
+                0x20,
+                0x40,
+                0x40,
+            ],
+            [
+                0xF0,
+                0x90,
+                0xF0,
+                0x90,
+                0xF0,
+            ],
+            [
+                0xF0,
+                0x90,
+                0xF0,
+                0x10,
+                0xF0
+            ],
+            [
+                0xF0,
+                0x90,
+                0xF0,
+                0x90,
+                0x90,
+            ],
+            [
+                0xE0,
+                0x90,
+                0xE0,
+                0x90,
+                0xE0,
+            ],
+            [
+                0xF0,
+                0x80,
+                0x80,
+                0x80,
+                0xF0,
+            ],
+            [
+                0xE0,
+                0x90,
+                0x90,
+                0x90,
+                0xE0,
+            ],
+            [
+                0xF0,
+                0x80,
+                0xF0,
+                0x80,
+                0xF0,
+            ],
+            [
+                0xF0,
+                0x80,
+                0xF0,
+                0x80,
+                0x80,
+            ]
+        ];
+
+        let mut i = 0;
+        for sprite in sprites.iter() {
+            for ch in sprite {
+                memory.mem[i] = *ch
+            }
         }
+
+
+        memory
     }
 }
